dcf4eb92fc9de156987ffd11ca8547552224c94f
jpswt <joel_swt@yahoo.com>
2022-08-12 10:45:19 -0500

    first commit, all done

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
-import cookie from 'cookie'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import React from 'react';
+import { Routes, Route, Navigate, Outlet } from 'react-router';
+import cookie from 'cookie';
+import Home from './components/Home';
+import About from './components/About';
+import Car from './components/Car';
+import Login from './components/Login';
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+	const cookies = cookie.parse(document.cookie);
+	return cookies['loggedIn'] ? true : false;
+};
 
 // Write ProtectedRoute function here
-
+const ProtectedRoutes = () => {
+	return checkAuth() ? <Outlet /> : <Navigate to="/login" />;
+};
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route element={<ProtectedRoutes />}>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/car/:id" element={<Car />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default Router;
